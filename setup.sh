d36a9839378e599a2d96dcfd83e97840174444d6
Bengt Martensson <barf@bengt-martensson.de>
2020-09-26 11:17:01 +0200

    Added download (-d) option. Minor fixes.

--- setup.sh
+++ setup.sh
@@ -2,13 +2,15 @@
 
 RMHOME=/usr/local/rmir
 BIN=/usr/local/bin
-desktopdir=$HOME/.local/share/applications
+DESKTOPDIR=$HOME/.local/share/applications
+URL=https://sourceforge.net/projects/controlremote/files/latest/download
+DOWNLOAD=/tmp/rmir$$.zip
 
 fixdesktop()
 {
     TEMPFILE=/tmp/${1}$$
-    sed -e "s|Exec=.*|Exec=${BIN}/${2}|" -e "s/Categories=.*/Categories=AudioVideo;Java;/" < ${desktopdir}/$1.desktop > ${TEMPFILE}
-    mv ${TEMPFILE} ${desktopdir}/$1.desktop
+    sed -e "s|Exec=.*|Exec=${BIN}/${2}|" -e "s/Categories=.*/Categories=AudioVideo;Java;/" < ${DESKTOPDIR}/$1.desktop > ${TEMPFILE}
+    mv ${TEMPFILE} ${DESKTOPDIR}/$1.desktop
 }
 
 HERE="$(dirname -- "$(readlink -f -- "${0}")" )"
@@ -17,17 +19,26 @@ if [ ! -d ${RMHOME} ] ; then
     mkdir ${RMHOME}
 fi
 
+if [ "$1" = "-d" ] ; then
+    wget -O $DOWNLOAD $URL
+    ZIP=$DOWNLOAD
+fi
+
 if [ -f "$1" ] ; then
     ZIP=$(readlink -f -- $1)
+fi
+
+if [ -f "$ZIP" ] ; then 
     cd ${RMHOME}
     rm -rf *
-    unzip ${ZIP}
+    unzip -q ${ZIP}
     sh ./setup.sh
 fi
 
 install ${HERE}/rmir.sh ${RMHOME}
 ln -sf ${RMHOME}/rmir.sh ${BIN}/rmir
 ln -sf ${RMHOME}/rmir.sh ${BIN}/remotemaster
+ln -sf ${RMHOME}/rmir.sh ${BIN}/rmdu
 ln -sf ${RMHOME}/rmir.sh ${BIN}/rmpb
 
 fixdesktop RemoteMaster remotemaster
